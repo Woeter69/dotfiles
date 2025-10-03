6515367c3e688326abd8b8a9b2bc82e5311564ce
Woeter <skippedlead@gmail.com>
2025-10-03 20:41:49 +0530

    Added psqlrc

--- setup/setup-wsl-dotfiles.sh
+++ setup/setup-wsl-dotfiles.sh
@@ -2,7 +2,7 @@
 sudo apt update && sudo apt upgrade -y
 
 # Install essentials
-sudo apt install -y zsh git curl wget fzf neovim
+sudo apt install -y zsh git curl wget fzf neovim postgresql
 
 # Install Oh My Zsh non-interactively
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
@@ -23,5 +23,7 @@ fi
 ln -sf ~/dotfiles/.zshrc ~/.zshrc
 mkdir -p ~/.config
 ln -sf ~/dotfiles/nvim ~/.config/nvim
+ln -sf ~/dotfiles/.psqlrc ~/.psqlrc
 
+zsh
 echo "Zsh + Neovim setup complete. Open a new WSL session to start using your dotfiles."
