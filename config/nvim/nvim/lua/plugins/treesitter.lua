ecfaaff00be3fdccfdd4ba3b021168ee792f898d
Woeter <skippedlead@gmail.com>
2025-10-27 18:04:00 +0530

    Removed nvim from root

--- config/nvim/nvim/lua/plugins/treesitter.lua
+++
@@ -1,5 +0,0 @@
-require'nvim-treesitter.configs'.setup {
-  ensure_installed = { "html", "css", "javascript" },  -- languages you want
-  highlight = { enable = true },                       -- syntax highlighting
-  auto_install = true,                                  -- install missing parsers automatically
-}
